5d4fdc6cfc80089a914e4046549e239e05445ec1
busyrat <1535858847@qq.com>
2024-07-19 17:51:58 +0800

    拦截客户端导航，获取目标路由的JSX对象，使用 react-dom/client 的 hydrateRoot 方法更新

--- public/client.js
+++ public/client.js
@@ -1,18 +1,27 @@
 console.log('use client')
+import { hydrateRoot } from 'react-dom/client';
+
+function parseJSX(key, value) {
+  if (value === "$RE") {
+    return Symbol.for("react.element");
+  } else if (typeof value === "string" && value.startsWith("$$")) {
+    return value.slice(1);
+  } else {
+    return value;
+  }
+}
+
+const root = hydrateRoot(document, JSON.parse(window.__INITIAL_CLIENT_JSX_STRING__, parseJSX));
 
 async function navigate (pathname) {
   console.log('navigate', pathname)
-  const response = await fetch(pathname)
-  const html = await response.text()
-  //  获取其中的 body 标签内容
-  const res = /<body(.*?)>/.exec(html);
-  const bodyStartIndex = res.index + res[0].length
-  const bodyEndIndex = html.lastIndexOf("</body>");
-  const bodyHTML = html.slice(bodyStartIndex, bodyEndIndex);
-  console.log(bodyHTML);
-  // 简单粗暴的直接替换 HTML
-  document.body.innerHTML = bodyHTML;
-  bindEvent()
+  const response = await fetch(pathname, {
+    headers: new Headers({
+      'content-type': 'application/json',
+    })
+  })
+  const jsx = await response.text()
+  root.render(JSON.parse(jsx, parseJSX));
 }
 
 function bindEvent() {
@@ -32,3 +41,25 @@ window.addEventListener("popstate", () => {
 });
 
 bindEvent()
+
+import React from 'react';
+const postContent = ''
+const Link = ({ href, children }) => {
+  return React.createElement("a", { href: href, className: "link text-blue-500 hover:underline" }, children);
+};
+const Nav = () => React.createElement("nav", { className: "p-10 text-xl bg-blue-200 flex gap-10" },
+  React.createElement(Link, { href: "/hello" }, "Hello"),
+  React.createElement(Link, { href: "/world" }, "World"));
+const Footer = () => React.createElement("div", { className: "px-10 py-4 text-xl bg-blue-200" }, "footer");
+let jsx = (React.createElement("html", { lang: "en" },
+  React.createElement("head", null,
+      React.createElement("meta", { charSet: "UTF-8" }),
+      React.createElement("meta", { name: "viewport", content: "width=device-width, initial-scale=1.0" }),
+      React.createElement("title", null, "no.2"),
+      React.createElement("script", { src: "https://cdn.tailwindcss.com" })),
+  React.createElement("body", { className: "flex flex-col h-screen" },
+      React.createElement(Nav, null),
+      React.createElement("input", { type: "text", className: 'mx-10 my-4 border' }),
+      React.createElement("div", { className: "m-10 flex-1" }, postContent),
+      React.createElement(Footer, null))));
+console.log(jsx);
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,56 @@ import { renderToString } from 'react-dom/server'
 
 const app = express()
 
-console.log();
-
 app.use(express.static(path.join(__dirname, '../public')));
+export async function renderJSXToClientJSX(jsx) {
+  if (
+    typeof jsx === "string" ||
+    typeof jsx === "number" ||
+    typeof jsx === "boolean" ||
+    jsx == null
+  ) {
+    return jsx;
+  } else if (Array.isArray(jsx)) {
+    return Promise.all(jsx.map((child) => renderJSXToClientJSX({...child})));
+  } else if (jsx != null && typeof jsx === "object") {
+    if (jsx.$$typeof === Symbol.for("react.element")) {
+      if (typeof jsx.type === "string") {
+        return {
+          ...jsx,
+          props: await renderJSXToClientJSX(jsx.props),
+        };
+      } else if (typeof jsx.type === "function") {
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = await Component(props);
+        return renderJSXToClientJSX(returnedJsx);
+      } else throw new Error("Not implemented.");
+    } else {      
+      return Object.fromEntries(
+        await Promise.all(
+          Object.entries(jsx).map(async ([propName, value]) => [
+            propName,
+            await renderJSXToClientJSX(value),
+          ])
+        )
+      );
+    }
+  } else throw new Error("Not implemented");
+}
 
-function htmlGenerator(postContent) {
+
+export function stringifyJSX(key, value) {
+  if (value === Symbol.for("react.element")) {
+    // We can't pass a symbol, so pass our magic string instead.
+    return "$RE"; // Could be arbitrary. I picked RE for React Element.
+  } else if (typeof value === "string" && value.startsWith("$")) {
+    // To avoid clashes, prepend an extra $ to any string already starting with $.
+    return "$" + value;
+  } else {
+    return value;
+  }
+}
+async function contentGenerator(postContent) {
   const Link = ({ href, children }) => {
     return <a href={href} className="link text-blue-500 hover:underline">{children}</a>
   }
@@ -19,7 +64,7 @@ function htmlGenerator(postContent) {
     <Link href="/world">World</Link>
   </nav>
   const Footer = () => <div className="px-10 py-4 text-xl bg-blue-200">footer</div>
-  const jsx = (
+  let jsx = (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
@@ -29,25 +74,54 @@ function htmlGenerator(postContent) {
       </head>
       <body className="flex flex-col h-screen">
         <Nav />
-        <div className="p-10 flex-1">{ postContent }</div>
+        <input type="text" className='mx-10 my-4 border' />
+        <div className="m-10 flex-1">{ postContent }</div>
         <Footer />
       </body>
-      <script src="/client.js"></script>
     </html>
   )
 
-  return renderToString(jsx)
+  let html = renderToString(jsx)
+  jsx = await renderJSXToClientJSX(jsx)
+  jsx = JSON.stringify(jsx, stringifyJSX)
+
+  
+  html += `
+      <script>
+        window.__INITIAL_CLIENT_JSX_STRING__ = ${JSON.stringify(jsx)}
+      </script>
+      <script type="importmap">
+        {
+          "imports": {
+            "react": "https://esm.sh/react@18.2.0",
+            "react-dom/client": "https://esm.sh/react-dom@18.2.0/client?dev"
+          }
+        }
+      </script>
+      <script type="module" src="/client.js"></script>
+  `
+
+  return { html, jsx }
 }
+
+// 过滤 favicon 请求
+app.use('/favicon.ico', (req, res, next) => {
+  res.status(204).end(); // 返回204 No Content
+});
 
 app.get('/:route(*)', async (req, res) => {
   const route = req.params.route
   const url = new URL(req.url, `http://${req.headers.host}`);
-  // console.log(req.url, url);
   
   try {
     const postContent = await readFile(`./posts/${route}.md`, 'utf8')
-    res.setHeader("Content-Type", "text/html");
-    res.send(htmlGenerator(postContent))
+    if (req.headers['content-type'] === 'application/json') {
+      res.setHeader("content-type", "application/json");
+      res.send((await contentGenerator(postContent)).jsx)
+    } else {
+      res.setHeader("content-type", "text/html");
+      res.send((await contentGenerator(postContent)).html)
+    }
   } catch (error) {
     res.send('404')
     return
